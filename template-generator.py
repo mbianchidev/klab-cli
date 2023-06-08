a6c0453ceddff821c8b9dc7886ce5f7b945d0f21
Daniel Ilievski <ilievskiot@hotmail.com>
2023-06-08 07:36:41 +0200

    Update terraform generation, remove Kubernetes & CloudFormation

--- template-generator.py
+++ template-generator.py
@@ -5,103 +5,55 @@ import yaml
 from jinja2 import Environment, FileSystemLoader
 
 logging.basicConfig(level=logging.INFO)
-# TODO it needs to be completely reworked
+
 
 class TemplateGenerator:
     CUR_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
-    DIR_PATH_TF = os.path.abspath(os.path.join(CUR_DIR_PATH, '..', 'Modules', 'Terraform'))
-    DIR_PATH_KB = os.path.abspath(os.path.join(CUR_DIR_PATH, '..', 'Modules', 'Kubernetes'))
-    DIR_PATH_CF = os.path.abspath(os.path.join(CUR_DIR_PATH, '..', 'Modules', 'CloudFormation'))
+    TESTING_DIR = os.path.abspath(os.path.join(CUR_DIR_PATH, 'Testing'))
 
-    @staticmethod
-    def gen_tf_template(template):
+    def gen_tf_template(self, cloud_provider, module, conf_file):
         """
         Generate Terraform template.
 
-        :param template: Name of the template file
+        :param cloud_provider: Cloud provider name
+        :param module: Module name
+        :param conf_file: Name of the configuration file
         :return: Generated Terraform template content
         """
         try:
-            with open(os.path.join(TemplateGenerator.DIR_PATH_TF, 'conf', template)) as file:
+            dir_path_tf = os.path.abspath(os.path.join(TemplateGenerator.CUR_DIR_PATH, cloud_provider, module))
+
+            with open(os.path.join(dir_path_tf, conf_file)) as file:
                 conf_var = yaml.safe_load(file)
 
-            file_loader = FileSystemLoader([os.path.join(TemplateGenerator.DIR_PATH_TF, 'templates')])
+            file_loader = FileSystemLoader([dir_path_tf])
 
             env = Environment(loader=file_loader, autoescape=True)
 
             jinja_template = env.get_template(conf_var['template_path'])
             output = jinja_template.render(conf_var)
 
-            with open(os.path.join(TemplateGenerator.DIR_PATH_TF, 'output-templates', 'output-tf.tf'), 'w') as f:
+            if not os.path.exists(TemplateGenerator.TESTING_DIR):
+                os.makedirs(TemplateGenerator.TESTING_DIR)
+
+            with open(os.path.join(TemplateGenerator.TESTING_DIR, f'{conf_file}-output-tf.tf'), 'w') as f:
                 f.write(output)
+
             return output
         except Exception as err:
             logging.exception("Can't generate Terraform template")
             return str(err)
 
-    @staticmethod
-    def gen_kub_template(template):
-        """
-        Generate Kubernetes template.
-
-        :param template: Name of the template file
-        :return: Generated Kubernetes template content
-        """
-        try:
-            with open(os.path.join(TemplateGenerator.DIR_PATH_KB, 'conf', template)) as file:
-                conf_var = yaml.safe_load(file)
-
-            file_loader = FileSystemLoader([os.path.join(TemplateGenerator.DIR_PATH_KB, 'templates')])
-
-            env = Environment(loader=file_loader, autoescape=True)
-
-            jinja_template = env.get_template(conf_var['template_path'])
-            output = jinja_template.render(conf_var)
-
-            with open(os.path.join(TemplateGenerator.DIR_PATH_KB, 'output-templates', 'output-kb.tf'), 'w') as f:
-                f.write(output)
-            return output
-        except Exception as err:
-            logging.exception("Can't generate Kubernetes template")
-            return str(err)
-
-    @staticmethod
-    def gen_cf_template(template):
-        """
-        Generate CloudFormation template.
-
-        :param template: Name of the template file
-        :return: Generated CloudFormation template content
-        """
-        try:
-            with open(os.path.join(TemplateGenerator.DIR_PATH_CF, 'conf', template)) as file:
-                conf_var = yaml.safe_load(file)
-
-            file_loader = FileSystemLoader([os.path.join(TemplateGenerator.DIR_PATH_CF, 'templates')])
-
-            env = Environment(loader=file_loader, autoescape=True)
-
-            jinja_template = env.get_template(conf_var['template_path'])
-            output = jinja_template.render(conf_var)
-
-            with open(os.path.join(TemplateGenerator.DIR_PATH_CF, 'templates', 'CodePipeline', 'output.yaml'), 'w') as f:
-                f.write(output)
-            return output
-        except Exception as err:
-            logging.exception("Can't generate CloudFormation template")
-            return str(err)
-
-    @staticmethod
-    def parse_arguments():
+    def parse_arguments(self):
         """
         Parse command-line arguments.
 
         :return: Parsed arguments
         """
         parser = argparse.ArgumentParser(description='Generate or deploy AWS Stack')
+        parser.add_argument("--cloud-provider", "-c", help="Cloud provider name", action="store")
+        parser.add_argument("--module", "-m", help="Module name", action="store")
         parser.add_argument("--terraform", "-tf", help="Generate Terraform template", action="store")
-        parser.add_argument("--kubernetes", "-k", help="Generate Kubernetes template", action="store")
-        parser.add_argument("--cloudformation", "-cf", help="Generate CloudFormation template", action="store")
         return parser.parse_args()
 
     def run(self):
@@ -111,14 +63,10 @@ class TemplateGenerator:
         args = self.parse_arguments()
         logging.info(args)
 
-        if args.terraform:
-            print(TemplateGenerator.gen_tf_template(args.terraform))
-        elif args.kubernetes:
-            print(TemplateGenerator.gen_kub_template(args.kubernetes))
-        elif args.cloudformation:
-            print(TemplateGenerator.gen_cf_template(args.cloudformation))
+        if args.cloud_provider and args.module and args.terraform:
+            self.gen_tf_template(args.cloud_provider, args.module, args.terraform)
         else:
-            logging.info("Choose an option")
+            logging.info("Incomplete arguments. Please provide cloud provider, module, and configuration file.")
 
 
 if __name__ == '__main__':
