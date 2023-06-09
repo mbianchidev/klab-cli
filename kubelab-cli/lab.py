f3d2ee3212f4ecdd9d430a65a06b2e673d3cf359
BlagojVazliski <blagoj@kubelab.cloud>
2023-06-09 12:53:33 +0200

    Moving kubecli to middleware

---
+++ kubelab-cli/lab.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+
+import click
+import boto3
+import os
+import argparse
+import logging
+import subprocess
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('-cp', '--credentials-provider', type=str, help='Credentials provider')
+def init(credentials_provider):
+    if credentials_provider == 'AWS':
+        # Check if the AWS credentials file exists
+        aws_credentials_file = os.path.expanduser('~/.aws/credentials')
+        if os.path.isfile(aws_credentials_file):
+            # Read the AWS credentials from the file
+            aws_credentials = dict()
+            with open(aws_credentials_file, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.strip()
+                    if line.startswith('aws_access_key_id'):
+                        aws_credentials['aws_access_key_id'] = line.split('=')[1].strip()
+                    elif line.startswith('aws_secret_access_key'):
+                        aws_credentials['aws_secret_access_key'] = line.split('=')[1].strip()
+
+            # Save the credentials to a file
+            credential_file_path = 'credentials/kube_credential.txt'
+            with open(credential_file_path, 'w') as f:
+                f.write(f"AWS Access Key ID: {aws_credentials['aws_access_key_id']}\n")
+                f.write(f"AWS Secret Access Key: {aws_credentials['aws_secret_access_key']}\n")
+                click.echo(f'Credentials saved to {credential_file_path}')
+        else:
+            click.echo('AWS credentials file not found. Please enter the credentials.')
+            aws_access_key_id = click.prompt('AWS Access Key ID')
+            aws_secret_access_key = click.prompt('AWS Secret Access Key', hide_input=True)
+
+            # Save the credentials to a file
+            credential_file_path = 'credential/kube_credential.txt'
+            with open(credential_file_path, 'w') as f:
+                f.write(f"AWS Access Key ID: {aws_access_key_id}\n")
+                f.write(f"AWS Secret Access Key: {aws_secret_access_key}\n")
+                click.echo(f'Credentials saved to {credential_file_path}')
+
+            session = boto3.Session(
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key
+            )
+    else:
+        click.echo('Unsupported credentials provider.')
+
+
+# def open_file(ctx, param, value):
+#     if value:
+#         return open(value, 'r')
+#     else:
+#         return None
+
+
+class TemplateGenerator:
+    CUR_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+    TESTING_DIR = os.path.abspath(os.path.join(CUR_DIR_PATH, 'Testing'))
+
+
+@cli.command()
+@click.option('--file', type=click.Path(exists=True), help='Path to the config file in yaml format',)
+@click.argument('param_type', type=click.Choice(['cluster', 'role', 'rbac']))
+@click.argument('cloud_provider', type=click.Choice(['AWS', 'AZURE', 'GCP']))
+@click.argument('module', type=click.Choice(['EKS', 'EBS', 'S3']))
+def create(param_type, cloud_provider, module, file):
+    if file is None:
+        click.echo('Config file not provided.')
+    else:
+        if param_type == 'role':
+            click.echo("This feature will be available soon")
+        elif param_type == 'cluster':
+            try:
+                dir_path_tf = os.path.abspath(os.path.join(TemplateGenerator.CUR_DIR_PATH, cloud_provider, module))
+                conf_path = os.path.join(dir_path_tf, f'{file}')
+
+                with open(conf_path) as file:
+                    conf_var = yaml.safe_load(file)
+
+                file_loader = FileSystemLoader(dir_path_tf)
+
+                env = Environment(loader=file_loader, autoescape=True)
+
+                jinja_template = env.get_template(conf_var['template_path'])
+                output = jinja_template.render(conf_var)
+
+                os.makedirs(TemplateGenerator.TESTING_DIR, exist_ok=True)
+                output_path = os.path.join(TemplateGenerator.TESTING_DIR, 'output-tf.tf')
+                with open(output_path, 'w') as f:
+                    f.write(output)
+                print("Your template is created")
+                ter_dir = os.path.join(TemplateGenerator.TESTING_DIR)
+                os.chdir(ter_dir)
+                subprocess.run(['terraform', 'init'])
+                subprocess.run(['terraform', 'plan'])
+                return output
+            except Exception as err:
+                logging.exception("Can't generate Terraform template")
+                return str(err)
+            # if file.name == 'eks_cluster.yaml':
+            #     print("Creating cluster with conf from this file: ", file.name)
+            #     subprocess.run(['python3', 'template-generator.py', '--tf', file.name])
+            
+            #     output_template_dir = 'AWS/EKS/output-template'
+            #     os.chdir(output_template_dir)
+            #     print("New dir is: ", output_template_dir)
+            
+            #     subprocess.run(['terraform', 'init'])
+            #     subprocess.run(['terraform', 'plan'])
+            # elif file.name == 'ebs_volume.yaml':
+            #     print("This module is in progress")
+            #     subprocess.run(['python3', 'template-generator.py', '--tf', file.name])
+            
+            #     output_template_dir = 'AWS/EBS/output-template'
+            #     os.chdir(output_template_dir)
+            #     print("New dir is: ", output_template_dir)
+                
+            #     subprocess.run(['terraform', 'init'])
+            #     subprocess.run(['terraform', 'plan'])
+            # elif file.name == 's3.yaml':
+            #     print("This module is in progress")
+            #     subprocess.run(['python3', 'template-generator.py', '--tf', file.name])
+            
+            #     output_template_dir = 'AWS/S3/output-template'
+            #     os.chdir(output_template_dir)
+            #     print("New dir is: ", output_template_dir)
+                
+            #     subprocess.run(['terraform', 'init'])
+            #     subprocess.run(['terraform', 'plan'])
+            # elif file.name == 'api_gateway.yaml':
+            #     print("This module is in progress")
+            #     subprocess.run(['python3', 'template-generator.py', '--tf', file.name])
+            
+            #     output_template_dir = 'AWS/API-Gateway/output-template'
+            #     os.chdir(output_template_dir)
+            #     print("New dir is: ", output_template_dir)
+                
+            #     subprocess.run(['terraform', 'init'])
+            #     subprocess.run(['terraform', 'plan'])
+            # elif file.name == 'route53.yaml':
+            #     print("This module is in progress")
+            #     subprocess.run(['python3', 'template-generator.py', '--tf', file.name])
+            
+            #     output_template_dir = 'AWS/Route53/output-template'
+            #     os.chdir(output_template_dir)
+            #     print("New dir is: ", output_template_dir)
+                
+            #     subprocess.run(['terraform', 'init'])
+            #     subprocess.run(['terraform', 'plan'])
+            # elif file.name == 'vpc.yaml':
+            #     print("This module is in progress")
+            #     subprocess.run(['python3', 'template-generator.py', '--tf', file.name])
+            
+            #     output_template_dir = 'AWS/VPC/output-template'
+            #     os.chdir(output_template_dir)
+            #     print("New dir is: ", output_template_dir)
+                
+            #     subprocess.run(['terraform', 'init'])
+            #     subprocess.run(['terraform', 'plan'])
+        elif param_type == 'rbac':
+            click.echo("This feature future will be available soon")
+            
+
+if __name__ == '__main__':
+    cli()
