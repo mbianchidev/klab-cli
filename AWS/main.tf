0713eac27bd0cd4f5619aa6734ef68807cbd44a5
BlagojVazliski <blagoj@kubelab.cloud>
2023-06-12 17:54:12 +0200

    VPC moudle modifyed with plain terrafrom

--- AWS/main.tf
+++ AWS/main.tf
@@ -1,11 +1,24 @@
-module "EBS" {
-  source = "./EBS"
+# module "EBS" {
+#   source = "./EBS"
+# }
+
+# module "S3" {
+#   source = "./S3"
+  
+# }
+module "VPC" {
+  source = "./VPC"
 }
 
-module "S3" {
-  source = "./S3"
-  
+output "subnet_public" {
+  value = module.VPC.public_id
 }
-output "ebs_ids" {
-  value = module.EBS.ebs_id
+output "subnet_private" {
+  value = module.VPC.private_id
 }
+output "security_group_id" {
+  value = module.VPC.security_group_id
+}
+# output "ebs_ids" {
+#   value = module.EBS.ebs_id
+# }
