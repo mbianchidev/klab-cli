838357dfd0b863668c5d6f5fbd047ddce6e92775
BlagojVazliski <blagoj@kubelab.cloud>
2023-06-16 14:34:17 +0200

    AKS and VNet module connected to work together

--- Azure/AKS/variables.tf
+++ Azure/AKS/variables.tf
@@ -1,5 +1,11 @@
-variable "rgname" {
-    default = "aks-tg"
+variable "akspodssubnet_id" {
+  description = "AKS Pod Subnet ID"
+}
+variable "resource_group_name" {
+  description = "Resource group name"
+}
+variable "resource_group_location" {
+  description = "Resource group location"
 }
 variable "cluster_name" {
   default = "K8Scluster"
@@ -13,62 +19,12 @@ variable "node_count" {
 variable "disk_size" {
   default = 30
 }
-variable "location" {
-  default = "eastus"
-}
-variable "vmsubnet" {
-  default = "10.0.0.0/24"
-}
-variable "akspodssubnet" {
-  default = "10.0.3.0/24"
-}
-variable "aksservicecidr" {
-  default = "10.1.0.0/16"
-}
-variable "dockercidrip" {
-  default = "172.17.0.1/16"
+variable "vmsize" {
+  default = "Standard_DS2_v2"
 }
 variable "aksdns" {
   default = "10.1.0.10"
 }
-variable "vmsize" {
-  default = "Standard_DS2_v2"
-}
-variable "nsg_rules" {
-  description = "List of security rules for the network security group"
-  type        = list(object({
-    name                       = string
-    priority                   = number
-    direction                  = string
-    access                     = string
-    protocol                   = string
-    source_port_range          = string
-    destination_port_range     = string
-    source_address_prefix      = string
-    destination_address_prefix = string
-  }))
-  default = [
-    {
-      name                       = "nsg-web_port"
-      priority                   = 100
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "80"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    },
-    {
-      name                       = "nsg-ssh"
-      priority                   = 200
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "22"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-  ]
+variable "aksservicecidr" {
+  default = "10.1.0.0/16"
 }
--- Azure/main.tf
+++ Azure/main.tf
@@ -1,3 +1,9 @@
+module "VNet" {
+  source = "./VNet"
+}
 module "AKS" {
   source = "./AKS"
+  resource_group_location = module.VNet.resource_group_location
+  resource_group_name = module.VNet.resource_group_name
+  akspodssubnet_id = module.VNet.akspodssubnet
 }
