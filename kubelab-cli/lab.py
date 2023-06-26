d540b3a89fdbee8524f31f52f2a45e6fb4ef83a6
Daniel Ilievski <ilievskiot@hotmail.com>
2023-06-26 15:45:42 +0200

    Save the state of the init function in a file

--- kubelab-cli/lab.py
+++ kubelab-cli/lab.py
@@ -24,7 +24,7 @@ def init(ctx, cp):
         aws_credentials_file = os.path.expanduser('~/.aws/credentials')
         if os.path.isfile(aws_credentials_file):
             # Read the AWS credentials from the file
-            aws_credentials = dict()
+            aws_credentials = {}
             with open(aws_credentials_file, 'r') as f:
                 lines = f.readlines()
                 for line in lines:
@@ -39,18 +39,34 @@ def init(ctx, cp):
             with open(credential_file_path, 'w') as f:
                 f.write(f"AWS Access Key ID: {aws_credentials['aws_access_key_id']}\n")
                 f.write(f"AWS Secret Access Key: {aws_credentials['aws_secret_access_key']}\n")
-                click.echo(f'Credentials saved to {credential_file_path}')
+            click.echo(f'Credentials saved to {credential_file_path}')
+
+            # Save the state
+            state = {'initialized_cloud_provider': 'AWS'}
+            state_file_path = 'state/lab_state.json'
+            os.makedirs(os.path.dirname(state_file_path), exist_ok=True)  # Create 'state' folder if it doesn't exist
+            with open(state_file_path, 'w') as f:
+                json.dump(state, f)
+            click.echo(f'State saved to {state_file_path}')
         else:
             click.echo('AWS credentials file not found. Please enter the credentials.')
             aws_access_key_id = click.prompt('AWS Access Key ID')
             aws_secret_access_key = click.prompt('AWS Secret Access Key', hide_input=True)
 
             # Save the credentials to a file
-            credential_file_path = 'credential/aws_kube_credential.txt'
+            credential_file_path = 'credentials/aws_kube_credential.txt'
             with open(credential_file_path, 'w') as f:
                 f.write(f"AWS Access Key ID: {aws_access_key_id}\n")
                 f.write(f"AWS Secret Access Key: {aws_secret_access_key}\n")
-                click.echo(f'Credentials saved to {credential_file_path}')
+            click.echo(f'Credentials saved to {credential_file_path}')
+
+            # Save the state
+            state = {'initialized_cloud_provider': 'AWS'}
+            state_file_path = 'state/lab_state.json'
+            os.makedirs(os.path.dirname(state_file_path), exist_ok=True)  # Create 'state' folder if it doesn't exist
+            with open(state_file_path, 'w') as f:
+                json.dump(state, f)
+            click.echo(f'State saved to {state_file_path}')
 
             session = boto3.Session(
                 aws_access_key_id=aws_access_key_id,
@@ -68,6 +84,14 @@ def init(ctx, cp):
                 with open(credential_file_path, 'w') as f:
                     f.write(output)
                 click.echo(f'Credentials saved to {credential_file_path}')
+
+                # Save the state
+                state = {'initialized_cloud_provider': 'Azure'}
+                state_file_path = 'state/lab_state.json'
+                os.makedirs(os.path.dirname(state_file_path), exist_ok=True)  # Create 'state' folder if it doesn't exist
+                with open(state_file_path, 'w') as f:
+                    json.dump(state, f)
+                click.echo(f'State saved to {state_file_path}')
             else:
                 click.echo('Azure login failed. Please make sure Azure CLI is installed and logged in.')
         except Exception as e:
