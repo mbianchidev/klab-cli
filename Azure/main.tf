0c73b2cc060f3cda5e439e899be4bf04534ebc56
Daniel Ilievski <90925393+Ilievski-Daniel@users.noreply.github.com>
2023-06-27 17:09:25 +0200

    25/azure-cli (#28)
    * Update create function for Azure
    * Update terraform configuration for Azure credentials
    
    ---------
    
    Co-authored-by: BlagojVazliski <blagoj@kubelab.cloud>
    Co-authored-by: Matteo Bianchi <37507190+mbianchidev@users.noreply.github.com>

--- Azure/main.tf
+++ Azure/main.tf
@@ -1,9 +1,22 @@
+locals {
+  credentials_file = file("../kubelab-cli/credentials/azure_kube_credential.json")
+  credentials      = jsondecode(local.credentials_file)
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = local.credentials.id
+  tenant_id       = local.credentials.tenantId
+}
+
 module "VNet" {
   source = "./VNet"
 }
+
 module "AKS" {
-  source = "./AKS"
+  source                  = "./AKS"
   resource_group_location = module.VNet.resource_group_location
-  resource_group_name = module.VNet.resource_group_name
-  akspodssubnet_id = module.VNet.akspodssubnet
+  resource_group_name     = module.VNet.resource_group_name
+  akspodssubnet_id        = module.VNet.akspodssubnet
 }
