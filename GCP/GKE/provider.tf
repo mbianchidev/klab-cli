31839beaa066ebb8fde8b14fedc3b9e7db59731b
Daniel Ilievski <ilievskiot@hotmail.com>
2023-06-29 07:40:22 +0200

    Add required providers for the infrasturcture

---
+++ GCP/GKE/provider.tf
@@ -0,0 +1,12 @@
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
