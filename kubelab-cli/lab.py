f4162d4285c2ad328d8205f346260d259cfdc3d0
Daniel Ilievski <ilievskiot@hotmail.com>
2023-06-30 09:55:38 +0200

    Create init functionality for GCP

--- kubelab-cli/lab.py
+++ kubelab-cli/lab.py
@@ -91,6 +91,34 @@ def init(cp):
                 click.echo('Azure login failed. Please make sure Azure CLI is installed and logged in.')
         except Exception as e:
             click.echo(f'Error occurred while retrieving Azure credentials: {str(e)}')
+    elif cp == 'GCP':
+        try:
+            # Retrieve the GCP application default credentials file path
+            gcp_credentials_file = os.path.expanduser('~/.config/gcloud/application_default_credentials.json')
+            if os.path.isfile(gcp_credentials_file):
+                # Set the destination file path
+                credential_file_path = 'credentials/gcp_kube_credential.json'
+                os.makedirs(os.path.dirname(credential_file_path), exist_ok=True)
+                # Copy the GCP application default credentials file
+                with open(gcp_credentials_file, 'r') as src_file, open(credential_file_path, 'w') as dest_file:
+                    dest_file.write(src_file.read())
+
+                click.echo(f'Credentials saved to {credential_file_path}')
+
+                # Save the state
+                state = {'initialized_cloud_provider': 'GCP'}
+                state_file_path = 'state/lab_state.json'
+                os.makedirs(os.path.dirname(state_file_path), exist_ok=True)  # Create 'state' folder if it doesn't exist
+                with open(state_file_path, 'w') as f:
+                    json.dump(state, f)
+                click.echo(f'State saved to {state_file_path}')
+                print("Initializing Terraform...")
+                os.chdir('../GCP')
+                subprocess.run(['terraform', 'init'])
+            else:
+                click.echo('GCP application default credentials file not found. Please make sure you have authenticated with gcloud and have application default credentials.')
+        except Exception as e:
+            click.echo(f'Error occurred while retrieving GCP credentials: {str(e)}')
     else:
         click.echo('Unsupported credentials provider.')
 
