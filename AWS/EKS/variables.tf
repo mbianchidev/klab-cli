501d1405447b0f8d628f064b6e0a5a9758e1ef54
Daniel Ilievski <ilievskiot@hotmail.com>
2023-07-26 11:41:21 +0200

    Update AWS to be able to work with create function

--- AWS/EKS/variables.tf
+++ AWS/EKS/variables.tf
@@ -1,4 +1,3 @@
-
 variable "vpc_public_subnet_1" {
   description = "Public Subnet ID for EKS"
   type        = list(string)
@@ -13,25 +12,29 @@ variable "vpc_security_group" {
 }
 variable "endpoint_private_access" {
   description = "Variable for endpoint_private_access"
-  default = false 
+  default     = false
 }
 variable "endpoint_public_access" {
   description = "Variable for endpoint_public_access"
-  default = true
+  default     = true
 }
 variable "ami_type" {
   description = "Variable for AMI type"
-  default =   "AL2_x86_64"
+  default     = "AL2_x86_64"
 }
 variable "capacity_type" {
   description = "Variable for capacity type"
-  default =   "ON_DEMAND"
+  default     = "ON_DEMAND"
 }
 variable "disk_size" {
   description = "Variable for disk size"
-  default =   20
+  default     = 20
 }
 variable "instance_type" {
   description = "Variable for instance type"
-  default = "t3.medium"
+  default     = "t3.medium"
+}
+
+variable "cluster_name" {
+  type = string
 }
