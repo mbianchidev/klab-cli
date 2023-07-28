4e2a9d2e03bcb15b014bd98026bfb400cd42e4e7
Daniel Ilievski <ilievskiot@hotmail.com>
2023-07-28 10:09:01 +0200

    Create test install nginx deployment (not finished)

--- kubelab-cli/test/aws_nginx_test.py
+++ kubelab-cli/test/aws_nginx_test.py
@@ -1,8 +1,8 @@
 import subprocess
 import pytest
 import os
-import yaml
-
+import requests
+from kubernetes import client, config
 
 class TestAWSNginx:
     def test_init(self):
@@ -20,6 +20,42 @@ class TestAWSNginx:
         print("AWS create cluster result:", result.stdout)
         assert result.returncode == 0
         print("AWS cluster has been created!")
+
+    def test_install_nginx_deployment(self):
+        os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+        subprocess.run(['python3', 'lab.py', 'use', 'cluster', 'eks', '--provider', 'aws', '--region', 'us-west-2'])
+        subprocess.run(['python3', 'lab.py', 'add', 'deployment', 'nginx', '--version', '1.4.5'])
+
+        # Load Kubernetes configuration
+        config.load_kube_config()
+
+        # Create Kubernetes API client
+        api = client.CoreV1Api()
+
+        # Verify NGINX deployment
+        deployment_name = "nginx-deployment"  # Replace with your actual NGINX deployment name
+        namespace = "default"  # Replace with the appropriate namespace
+        try:
+            deployment = api.read_namespaced_deployment(deployment_name, namespace)
+        except client.ApiException as e:
+            assert False, f"NGINX deployment '{deployment_name}' not found in namespace '{namespace}'"
+
+        # Verify NGINX version (Assuming NGINX version is available in deployment annotations or labels)
+        # Example: assert deployment.metadata.annotations.get('nginx.version') == '1.4.5', "Incorrect NGINX version installed"
+
+        # Verify NGINX is accessible
+        nginx_service_name = "nginx-service"  # Replace with your actual NGINX service name
+        try:
+            service = api.read_namespaced_service(nginx_service_name, namespace)
+        except client.ApiException as e:
+            assert False, f"NGINX service '{nginx_service_name}' not found in namespace '{namespace}'"
+
+        # Temporary NGINX URL for testing (assuming the service is of type LoadBalancer)
+        nginx_test_url = f"http://{service.status.load_balancer.ingress[0].hostname}:80"
+
+        # Verify NGINX accessibility
+        response = requests.get(nginx_test_url)
+        assert response.status_code == 200, "NGINX is not accessible with HTTP 200"
 
 
 if __name__ == '__main__':
