0882e1a89beb917e74816b60c40575ad075f120d
Daniel Ilievski <ilievskiot@hotmail.com>
2023-08-04 13:44:16 +0200

    Add test install nginx deployment, update .gitignore

--- kubelab-cli/test/aws_nginx_test.py
+++ kubelab-cli/test/aws_nginx_test.py
@@ -17,45 +17,137 @@ class TestAWSNginx:
         print("Running test create cluster in AWS")
         os.chdir(os.path.join(os.path.dirname(__file__), '..'))
         result = subprocess.run(['python3', 'lab.py', 'create', 'cluster', '-pr', 'AWS'], capture_output=True, text=True)
+        time.sleep(700)
         assert result.returncode == 0
-        time.sleep(750)
         print("AWS cluster has been created!")
 
-    # def test_install_nginx_deployment(self):
-    #     os.chdir(os.path.join(os.path.dirname(__file__), '..'))
-    #     subprocess.run(['python3', 'lab.py', 'use', 'cluster', 'eks', '--provider', 'aws', '--region', 'us-west-2'])
-    #     subprocess.run(['python3', 'lab.py', 'add', 'deployment', 'nginx', '--version', 'latest-1'])
+    def test_use_aws_cluster(self):
+        print("Running test for use command for cluster in AWS")
+        os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+        result = subprocess.run(['python3', 'lab.py', 'use', 'cluster', 'eks', '--provider', 'AWS', '-r', 'eu-west-2'])
+        time.sleep(10)
+        assert result.returncode == 0
+        print("Use for AWS cluster has passed.")
+        
+    def install_nginx_deployment_aws(self):
+        print("Running test for add command in AWS")
+        os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+        result = subprocess.run(['python3', 'lab.py', 'add', 'nginx'], capture_output=True, text=True)
+        print("AWS add command result:", result.stdout)
+        time.sleep(10)
+        config.load_kube_config()
 
-    #     # Load Kubernetes configuration
-    #     config.load_kube_config()
+        # Create Kubernetes API client
+        api = client.AppsV1Api()
+        deployment_name = "nginx"  # Replace with your actual NGINX deployment name
+        namespace = "default"  # Replace with the appropriate namespace
+        try:
+            deployment = api.read_namespaced_deployment(deployment_name, namespace)
+        except client.ApiException as e:
+            assert False, f"NGINX deployment '{deployment_name}' not found in namespace '{namespace}'"
+        # Verify NGINX is accessible
+        api_core = client.CoreV1Api()
 
-    #     # Create Kubernetes API client
-    #     api = client.CoreV1Api()
+        nginx_service_name = "nginx"  # Replace with your actual NGINX service name
+        try:
+            service = api_core.read_namespaced_service(nginx_service_name, namespace)
+        except client.ApiException as e:
+            assert False, f"NGINX service '{nginx_service_name}' not found in namespace '{namespace}'"
 
-    #     # Verify NGINX deployment
-    #     deployment_name = "nginx-deployment"  # Replace with your actual NGINX deployment name
-    #     namespace = "default"  # Replace with the appropriate namespace
-    #     try:
-    #         deployment = api.read_namespaced_deployment(deployment_name, namespace)
-    #     except client.ApiException as e:
-    #         assert False, f"NGINX deployment '{deployment_name}' not found in namespace '{namespace}'"
+        nginx_test_ip = service.status.load_balancer.ingress[0].hostname
+        print(nginx_test_ip)
+        nginx_test_url = f"http://{nginx_test_ip}:80"
+        print(nginx_test_url)
+        
+        response = requests.get(nginx_test_url)
+        assert response.status_code == 200, "NGINX is not accessible with HTTP 200"
+        print("NGINX is installed and accessible with HTTP 200", response)
+        time.sleep(10)
+        assert result.returncode == 0
+    
+    def update_nginx_deployment_aws(self):
+        print("Running test for update command in AWS")
+        os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+        result = subprocess.run(['python3', 'lab.py', 'update', 'nginx', '--type=deployment', '--version=latest'], capture_output=True, text=True)
+        print("AWS update command result:", result.stdout)
+        config.load_kube_config()
 
-    #     # Verify NGINX version (Assuming NGINX version is available in deployment annotations or labels)
-    #     # Example: assert deployment.metadata.annotations.get('nginx.version') == 'latest-1', "Incorrect NGINX version installed"
+        # Create Kubernetes API client
+        api = client.AppsV1Api()
+        deployment_name = "nginx"
+        namespace = "default"
+        try:
+            deployment = api.read_namespaced_deployment(deployment_name, namespace)
+        except client.ApiException as e:
+            assert False, f"NGINX deployment '{deployment_name}' not found in namespace '{namespace}'"
 
-    #     # Verify NGINX is accessible
-    #     nginx_service_name = "nginx-service"  # Replace with your actual NGINX service name
-    #     try:
-    #         service = api.read_namespaced_service(nginx_service_name, namespace)
-    #     except client.ApiException as e:
-    #         assert False, f"NGINX service '{nginx_service_name}' not found in namespace '{namespace}'"
+        api_core = client.CoreV1Api()
 
-    #     # Temporary NGINX URL for testing (assuming the service is of type LoadBalancer)
-    #     nginx_test_url = f"http://{service.status.load_balancer.ingress[0].hostname}:80"
+        nginx_service_name = "nginx"
+        try:
+            service = api_core.read_namespaced_service(nginx_service_name, namespace)
+        except client.ApiException as e:
+            assert False, f"NGINX service '{nginx_service_name}' not found in namespace '{namespace}'"
 
-    #     # Verify NGINX accessibility
-    #     response = requests.get(nginx_test_url)
-    #     assert response.status_code == 200, "NGINX is not accessible with HTTP 200"
+        nginx_test_ip = service.status.load_balancer.ingress[0].ip
+        nginx_test_url = f"http://{nginx_test_ip}:80"
+
+        try:
+            response = requests.get(nginx_test_url)
+            assert response.status_code == 200, "NGINX is not accessible with HTTP 200"
+            print("NGINX is installed and accessible with HTTP 200", response)
+        except requests.exceptions.RequestException as e:
+            assert False, f"Error making request to NGINX: {e}"
+        assert result.returncode == 0
+        time.sleep(5)
+
+    def switch_nginx_deployment_aws(self):
+        print("Running test for switch to operator command in AWS")
+        os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+        subprocess.run(['python3', 'lab.py', 'add', 'nginx'])
+        # print("AWS add command result:", result.stdout)
+        time.sleep(10)
+        subprocess.run(['kubectl', 'apply', '-f', 'catalog/nginx/nginx_operator_test/nginx-ingress.yaml'])
+        time.sleep(10)
+        config.load_kube_config()
+
+        # Create Kubernetes API client
+        api = client.AppsV1Api()
+        deployment_name = "nginxingress-sample-nginx-ingress-controller"  # Replace with your actual NGINX deployment name
+        namespace = "default"  # Replace with the appropriate namespace
+        try:
+            deployment = api.read_namespaced_deployment(deployment_name, namespace)
+        except client.ApiException as e:
+            assert False, f"NGINX deployment '{deployment_name}' not found in namespace '{namespace}'"
+        # Verify NGINX is accessible
+        api_core = client.CoreV1Api()
+
+        nginx_service_name = "nginxingress-sample-nginx-ingress-controller"  # Replace with your actual NGINX service name
+        try:
+            service = api_core.read_namespaced_service(nginx_service_name, namespace)
+        except client.ApiException as e:
+            assert False, f"NGINX service '{nginx_service_name}' not found in namespace '{namespace}'"
+
+        nginx_test_ip = service.status.load_balancer.ingress[0].ip
+        nginx_test_url = f"http://{nginx_test_ip}:/nginx-health"
+
+        try:
+            response = requests.get(nginx_test_url)
+            assert response.status_code == 200, "NGINX is not accessible with HTTP 200"
+            print("NGINX is installed and accessible with HTTP 200", response)
+        except requests.exceptions.RequestException as e:
+            assert False, f"Error making request to NGINX: {e}"
+        # assert result.returncode == 0
+        time.sleep(10)
+
+    def test_delete_operator(self):
+        print("Running test for delete the operator in AWS")
+        os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+        result = subprocess.run(['python3', 'lab.py', 'delete', 'nginx', '--type=operator'], capture_output=True, text=True)
+        print("AWS delete operator result:", result.stdout)
+        time.sleep(5)
+        assert result.returncode == 0
+        print("Successfully deleted the operator")
 
     def test_destroy_aws_cluster(self):
         print("Running destroy cluster in AWS")
