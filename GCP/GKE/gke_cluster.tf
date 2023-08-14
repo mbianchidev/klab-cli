712454e619197af99e8df530a5ec0728cb4f404f
Daniel Ilievski <ilievskiot@hotmail.com>
2023-08-14 11:37:21 +0200

    Update GKE cluster to fix issue with node pool

--- GCP/GKE/gke_cluster.tf
+++ GCP/GKE/gke_cluster.tf
@@ -11,19 +11,19 @@ module "gke" {
   ip_range_services          = var.gke_ip_range_services_name
   horizontal_pod_autoscaling = var.gke_horizontal_pod_autoscaling
 
-  node_pools = [
-    {
-      name         = var.node_pool_name
-      machine_type = var.node_pool_machine_type
-      min_count    = var.node_pool_min_count
-      max_count    = var.node_pool_max_count
-      disk_size_gb = var.node_pool_disksize
-      preemptible  = var.node_pool_preemptible
-      auto_repair  = var.node_pool_auto_repair
-      auto_upgrade = var.node_pool_auto_upgrade
-      autoscaling  = var.node_pool_autoscaling
-    },
-  ]
+  # node_pools = [
+  #   {
+  #     name         = var.node_pool_name
+  #     machine_type = var.node_pool_machine_type
+  #     min_count    = var.node_pool_min_count
+  #     max_count    = var.node_pool_max_count
+  #     disk_size_gb = var.node_pool_disksize
+  #     preemptible  = var.node_pool_preemptible
+  #     auto_repair  = var.node_pool_auto_repair
+  #     auto_upgrade = var.node_pool_auto_upgrade
+  #     autoscaling  = var.node_pool_autoscaling
+  #   },
+  # ]
 }
 
 output "cluster_name" {
